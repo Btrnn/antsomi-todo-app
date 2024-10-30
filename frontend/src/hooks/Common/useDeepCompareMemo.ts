2283bc7c9950c5ff5656ee83d772ae3d5f6777a1
Truong Vi <nltruongvi@gmail.com>
2024-10-30 10:45:16 +0700

    fix: fix bug call maxium use effect

---
+++ frontend/src/hooks/Common/useDeepCompareMemo.ts
@@ -0,0 +1,22 @@
+import { useMemo, useRef } from 'react';
+import isEqual from 'react-fast-compare';
+
+function useDeepCompareMemoize(value: any) {
+  const ref = useRef();
+  // it can be done by using useMemo as well
+  // but useRef is rather cleaner and easier
+
+  if (!isEqual(value, ref.current)) {
+    ref.current = value;
+  }
+
+  return ref.current;
+}
+
+export function useDeepCompareMemo<T>(
+  callback: () => T,
+  dependencies: React.DependencyList | undefined,
+) {
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  return useMemo<T>(callback, dependencies?.map(useDeepCompareMemoize) as any);
+}
